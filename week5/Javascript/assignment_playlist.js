22695f0dade1d474551d91e13686b62aafb77bae
Waswafaith <waswafaith478@gmail.com>
2023-03-14 16:46:07 +0300

    Week 5

---
+++ week5/Javascript/assignment_playlist.js
@@ -0,0 +1,8 @@
+function playmusic1(){
+    var audio=document.getElementById("mymusic1");
+    audio.play();
+}
+function pausemusic1(){
+    var audio=document.getElementById("mymusic1");
+    audio.pause();
+} 
---
+++ week5/Javascript/index.js
@@ -0,0 +1,10 @@
+let age = 0
+let msg=""
+
+function add_numbers(num1,num2)
+{
+    return num1 + number2
+}
+add_numbers(23,45)
+console.log("Javascript Console testing")
+c
